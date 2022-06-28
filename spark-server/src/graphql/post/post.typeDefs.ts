b68b3ec105299485fdec97bb661ae16b82342772
lumpenop <swk5373@gmail.com>
2022-06-29 02:11:08 +0900

    Feat: getPostsByUser() 추가

--- spark-server/src/graphql/post/post.typeDefs.ts
+++ spark-server/src/graphql/post/post.typeDefs.ts
@@ -1,54 +1,59 @@
-import { gql } from 'apollo-server-express';
-import images from '../../models/image.model';
+import { gql } from "apollo-server-express";
+import images from "../../models/image.model";
 
 export default gql`
   type Post {
-    id:Int,
-    title:String,
-    post_content:String,
-    user_id:Int,
-    created_at:String,
-    hashtags:[Hashtag],
-    comments:[Comment],
-    writer:User,
-    likes:Int,
-    images:[Image]
+    id: Int
+    title: String
+    post_content: String
+    user_id: Int
+    created_at: String
+    hashtags: [Hashtag]
+    comments: [Comment]
+    writer: User
+    likes: Int
+    images: [Image]
   }
 
   type User {
-    id:Int,
-    nickname:String,
-    email:String
+    id: Int
+    nickname: String
+    email: String
   }
 
   type Comment {
-    id:Int,
-    comment:String,
-    post_id:Int,
-    user_id:Int
-    writer:User
+    id: Int
+    comment: String
+    post_id: Int
+    user_id: Int
+    writer: User
   }
 
-  type Hashtag{
-    id:Int,
-    hashtag:String
+  type Hashtag {
+    id: Int
+    hashtag: String
   }
 
-  type Image{
-    id:Int,
-    image_path:String,
-    post_id:Int
+  type Image {
+    id: Int
+    image_path: String
+    post_id: Int
   }
 
   type Query {
-    getPosts:[Post]
-    getPost(post_id:Int):Post
-    getPostsByHashtag(hashtag_id:Int):[Post]
+    getPosts: [Post]
+    getPost(post_id: Int): Post
+    getPostByUser(user_id: Int): [Post]
+    getPostsByHashtag(hashtag_id: Int): [Post]
   }
-  
+
   type Mutation {
-
-    createPost(title:String!, post_content:String!, user_id:Int, hashtags:[String], images:[String]):Int!
-
+    createPost(
+      title: String!
+      post_content: String!
+      user_id: Int
+      hashtags: [String]
+      images: [String]
+    ): Int!
   }
 `;
