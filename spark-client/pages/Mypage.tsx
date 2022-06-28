b1cb205fed8e6aabf8c0db231dbbcb73a3ba2746
lumpenop <swk5373@gmail.com>
2022-06-29 02:12:09 +0900

    Feat: MyPage에서 getPostsByUser 사용

--- spark-client/pages/Mypage.tsx
+++ spark-client/pages/Mypage.tsx
@@ -2,6 +2,9 @@ import MainList from "../components/MainList";
 import { useRecoilState } from "recoil";
 import { postsState } from "../states/spark";
 
+import { gql, useQuery } from "@apollo/client";
+import { GetPosts } from "../types/spark";
+
 // export async function getStaticProps() {
 //     const { data } = await client.query({
 //       query: gql`
@@ -25,12 +28,40 @@ import { postsState } from "../states/spark";
 //    };
 //   }
 
+const ALL_POST_BY_USER_ID = gql`
+  query GetPostByUser($userId: Int) {
+    getPostByUser(user_id: $userId) {
+      id
+      title
+      post_content
+      user_id
+      created_at
+      hashtags {
+        id
+        hashtag
+      }
+      comments {
+        post_id
+        user_id
+        comment
+      }
+      likes
+      writer {
+        nickname
+      }
+    }
+  }
+`;
+
+interface GetPostsByUser {
+  getPostByUser: GetPosts[];
+}
+
 export default function Mypage() {
   const [postData] = useRecoilState(postsState);
+  const { data, refetch } = useQuery<GetPostsByUser>(ALL_POST_BY_USER_ID, {
+    variables: { userId: 1 },
+  });
 
-  return (
-    <>
-      <MainList data={postData} />
-    </>
-  );
+  return <>{data && <MainList data={data.getPostByUser} />}</>;
 }
